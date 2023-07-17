b38ecc640a88ad85588d0f8e3cd8edb6007045ee
MohamedSalamaMouse <slamtm608@gmail.com>
2023-07-17 19:42:05 +0300

    usertype

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,4 +10,13 @@ class HomeController extends Controller
     public  function index(){
         return view('home');
     }
+    public function redirects(){
+        $usertype = auth()->user()->usertype;
+        if($usertype == '1'){
+            return view('admin.adminhome');
+        }else{
+            return view('home');
+
+        }
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get("/",[HomeController::class,'index']);
 
+Route::get("/",[HomeController::class,'index']);
+Route::get('redirects',[HomeController::class,'redirects']);
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
